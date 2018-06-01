2bdf1ec24ebf44084afe6b9ab2249b1a88746f81
TakanaNoHana <fall_legendary1992@yahoo.com.vn>
2018-06-01 08:02:35 +0700

    Initial commit

---
+++ dist/js/flickitty.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// external js: flickity.pkgd.js
+
+var $carousel = $('.carousel').flickity({
+  imagesLoaded: true,
+  percentPosition: false
+});
+
+var $imgs = $carousel.find('.carousel-cell .carousel-item');
+// get transform property
+var docStyle = document.documentElement.style;
+var transformProp = typeof docStyle.transform == 'string' ? 'transform' : 'WebkitTransform';
+// get Flickity instance
+var flkty = $carousel.data('flickity');
+
+$carousel.on('scroll.flickity', function () {
+  flkty.slides.forEach(function (slide, i) {
+    var img = $imgs[i];
+    var x = (slide.target + flkty.x) * -1 / 3;
+    img.style[transformProp] = 'translateX(' + x + 'px)';
+  });
+});
